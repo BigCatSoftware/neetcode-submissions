397f1cbfd20a05756c3b4a17428feac502d35182
Tiger <143446950+BigCatSoftware@users.noreply.github.com>
2026-06-09 13:04:16 -0700

    Add: is-anagram - submission-17

---
+++ Data Structures & Algorithms/is-anagram/submission-17.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
+
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[sArr[i] - 'a']++;
+            count[tArr[i] - 'a']--;
+        }
+
+        for (int num : count) {
+            if (num != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
